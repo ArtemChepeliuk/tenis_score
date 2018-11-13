a278b82c0730128966c69ce1507243259b83965f
artess <4epeliuk@gmail.com>
2018-11-13 16:59:24 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var leftScore=0;
+var rightScore=0;
+function lcounter(){
+    document.getElementById('lscore').innerHTML = ++leftScore;
+}
+function rcounter(){
+  document.getElementById('rscore').innerHTML = ++rightScore;     
+}
+
+// function reset() {
+//  let left=0;
+//  let right=0;
+//  document.getElementById('rscore').innerHTML = left; 
+//  document.getElementById('lscore').innerHTML = right;
+// }
