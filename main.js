09819622818427a68734694913a4de2b3bedacd6
artess <4epeliuk@gmail.com>
2018-11-13 22:40:40 +0200

    first commit

--- main.js
+++ main.js
@@ -1,35 +1,70 @@
-var leftScore = 0;
-var rightScore = 0;
+// var leftScore = 0;
+// var rightScore = 0;
+
+// function lcounter() {
+//   document.getElementById('lscore').innerHTML = ++leftScore;
+//   return leftScore;
+// }
+
+// function rcounter() {
+//   document.getElementById('rscore').innerHTML = ++rightScore;
+//   return rightScore;
+// }
+
+
+
+// function reset() {
+//   leftScore = 0;
+//   rightScore = 0;
+//   document.getElementById('lscore').innerHTML = leftScore;
+//   document.getElementById('rscore').innerHTML = rightScore;
+// }
+
+// document.getElementById("lbutton").addEventListener("click", sumNumbers)
+
+// function sumNumbers() {
+//   let x = document.getElementById('lscore').innerText;
+//   let y = document.getElementById('rscore').innerText;
+//   let summ = +x + +y;
+
+//   if (summ % 2 == 0) {
+//     document.getElementById('lBall').style.visibility = 'visible'
+//     document.getElementById('rBall').style.visibility = 'hidden'
+//   } else {
+//     document.getElementById('rBall').style.visibility = 'visible'
+//     document.getElementById('lBall').style.visibility = 'hidden'
+//   }
+// }
+
+var $leftScore = document.getElementById('lscore');
+  var $rightScore = document.getElementById('rscore');
+    leftSum = 0;
+    rightSum = 0;
 
 function lcounter() {
-  document.getElementById('lscore').innerHTML = ++leftScore;
-  return leftScore;
+  $leftScore.innerHTML = ++leftSum;;
+  onUpdate();
 }
 
 function rcounter() {
-  document.getElementById('rscore').innerHTML = ++rightScore;
-  return rightScore;
+  $rightScore.innerHTML = ++rightScore;
+  onUpdate();
 }
-
-
 
 function reset() {
-  leftScore = 0;
-  rightScore = 0;
-  document.getElementById('lscore').innerHTML = leftScore;
-  document.getElementById('rscore').innerHTML = rightScore;
+  $leftScore.innerHTML = leftSum = 0;
+  $rightScore.innerHTML = rightSum = 0;
+  onUpdate();
 }
 
-document.getElementById("lbutton").addEventListener("click", sumNumbers)
-
-function sumNumbers() {
-  let x = document.getElementById('lscore');
-  let y = document.getElementById('rscore');
-  let summ = x + y;
-
-  if (summ % 2 == 0) {
-    console.log(true)
+function onUpdate() {
+  if ((+leftSum + +rightSum) % 2) {
+    document.getElementById('lBall').style.visibility = 'visible'
+    document.getElementById('rBall').style.visibility = 'hidden'
+    // sum % 2 = 1
   } else {
-    console.log(false)
+    document.getElementById('rBall').style.visibility = 'visible'
+    document.getElementById('lBall').style.visibility = 'hidden'
+    // sum % 2 = 0
   }
 }
